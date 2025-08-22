f9e3e004b468d36dda4e280c94bf4c622c27ec3c
witam999 <witam999@gmail.com>
2025-08-22 09:36:50 +0800

    +

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,17 @@
 import { resolve } from "path";
 
 /* ---------- 第三方依赖 ---------- */
-import terser from "@rollup/plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
 
 // 配置
-export default [
-    {
-        output: [
-            {
-                file: resolve("lib", "main.d.ts"),
-                format: "es",
-            },
-        ],
-        plugins: [dts()],
-    },
-    {
-        output: ["es", "umd"].map((format) => ({
-            file: resolve("lib", `main.${format}.js`),
-            name: "uuid",
-            format,
-        })),
-        plugins: [commonjs(), terser(), typescript()],
-    },
-].map((config) => {
-    config.input = resolve("src", "main.ts");
-    return config;
-});
+export default {
+    input: resolve("src", "main.ts"),
+    output: ["es", "umd"].map((format) => ({
+        file: resolve("dist", `main.${format}.js`),
+        name: "default-proxy-handler",
+        format,
+    })),
+    plugins: [commonjs(), terser(), typescript()],
+};
